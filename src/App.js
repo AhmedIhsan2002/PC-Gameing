9f69c129a46cf3b45d0b23bea86280880f8a85c4
AhmedReact2023 <AhmedReactjs@gmail.com>
2023-08-11 13:51:03 +0300

    create file App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Signup from "./pages/Signup/index";
+import Login from "./pages/Login/index";
+
+import React, { Component } from "react";
+import Profile from "./pages/Profile";
+import UserList from "./pages/UserList";
+import UserDetails from "./pages/UserDetails/index";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+export default class App extends Component {
+  state = {
+    isAuthorized: false,
+    isAdmin: false,
+  };
+
+  componentDidMount() {
+    const token = localStorage.getItem("token");
+    if (token) {
+      this.setState({ isAuthorized: true });
+    }
+    const admin = localStorage.getItem("Admin");
+    if (admin !== "true") {
+      this.setState({ isAdmin: false });
+    } else {
+      this.setState({ isAdmin: true });
+    }
+  }
+
+  login = () => this.setState({ isAuthorized: true });
+
+  admin = () => {
+    this.setState({ isAdmin: true });
+  };
+
+  logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+    localStorage.removeItem("email");
+    localStorage.removeItem("Admin");
+
+    this.setState({ isAuthorized: false });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route index="true" element={<Navigate to="/login" />} />
+
+          <Route
+            path="/login"
+            element={
+              <>
+                {this.state.isAuthorized ? (
+                  <Navigate to="/controlPanel" />
+                ) : (
+                  <Login login={this.login} admin={this.admin} />
+                )}
+              </>
+            }
+          />
+
+          <Route
+            path="/signup"
+            element={
+              <>
+                {this.state.isAuthorized ? (
+                  <Navigate to="/controlPanel" />
+                ) : (
+                  <Signup login={this.login} />
+                )}
+              </>
+            }
+          />
+
+          <Route
+            path="/controlPanel"
+            element={
+              <ProtectedRoute
+                isAuthorized={this.state.isAuthorized}
+                logout={this.logout}
+                admin={this.state.isAdmin}
+              />
+            }
+          />
+          <Route
+            path="/profile"
+            element={<Profile admin={this.state.isAdmin} />}
+          />
+          <Route
+            path="userslist/:id"
+            element={
+              this.state.isAdmin ? (
+                <UserDetails />
+              ) : (
+                <Navigate to="/controlPanel" />
+              )
+            }
+          />
+          <Route
+            path="/userslist"
+            element={
+              this.state.isAdmin ? (
+                <UserList />
+              ) : (
+                <Navigate to="/controlPanel" />
+              )
+            }
+          />
+          <Route path="*" element={<h1>page not found 404</h1>} />
+        </Routes>
+      </div>
+    );
+  }
 }
-
-export default App;
