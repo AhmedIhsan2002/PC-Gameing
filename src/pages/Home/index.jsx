fdf98d9ee71da5a6c21e52a615e746f57643e8dc
AhmedReact2023 <AhmedReactjs@gmail.com>
2023-08-11 13:57:16 +0300

    create Folder Home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import Signup from "../../components/Signup";
+
+function Home() {
+  return (
+    <div className="home">
+      <Signup />
+    </div>
+  );
+}
+
+export default Home;
